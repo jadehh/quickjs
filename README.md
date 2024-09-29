243d01e3710b6f16cd001b6433fa9cbcfd7aaac0
jade <jadehh@live.com>
2024-09-29 14:27:02 +0800

    * 生成Windows动态库

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # quickjs
 quickjs
+
+
+## Windows动态库生成
+
+```bash
+cmake  -DCMAKE_BUILD_TYPE=Release "-DCMAKE_MAKE_PROGRAM=C:/Program Files/JetBrains/CLion 2023.2.1/bin/ninja/win/x64/ninja.exe" -G Ninja -B  cmake-build-release-mingw
+cmake --build cmake-build-release-mingw --target quickjs -j 10
+```
--- main.cpp
+++ main.cpp
@@ -4,6 +4,80 @@
 
 #include "./cxx/ffi.h"
 #include <cstring>
+
+jobject QuickJSWrapper::toJavaObject(JNIEnv *env, jobject thiz, JSValueConst& this_obj, JSValueConst& value) const{
+    jobject result;
+    switch (JS_VALUE_GET_NORM_TAG(value)) {
+        case JS_TAG_EXCEPTION: {
+            result = nullptr;
+            break;
+        }
+
+        case JS_TAG_STRING: {
+            result = toJavaString(env, context, value);
+            break;
+        }
+
+        case JS_TAG_BOOL: {
+            jvalue v;
+            v.z = static_cast<jboolean>(JS_VALUE_GET_BOOL(value));
+            result = env->CallStaticObjectMethodA(booleanClass, booleanValueOf, &v);
+            break;
+        }
+
+        case JS_TAG_INT: {
+            jvalue v;
+            v.j = static_cast<jint>(JS_VALUE_GET_INT(value));
+            result = env->CallStaticObjectMethodA(integerClass, integerValueOf, &v);
+            break;
+        }
+
+        case JS_TAG_BIG_INT: {
+            int64_t e;
+            JS_ToBigInt64(context, &e, value);
+            jvalue v;
+            v.j = e;
+            result = env->CallStaticObjectMethodA(longClass, longValueOf, &v);
+            break;
+        }
+
+        case JS_TAG_FLOAT64: {
+            jvalue v;
+            double d = JS_VALUE_GET_FLOAT64(value);
+            bool isInteger = floor(d) == d;
+            if (isInteger) {
+                v.j = static_cast<jlong>(d);
+                result = env->CallStaticObjectMethodA(longClass, longValueOf, &v);
+            } else {
+                v.d = static_cast<jdouble>(d);
+                result = env->CallStaticObjectMethodA(doubleClass, doubleValueOf, &v);
+            }
+            break;
+        }
+
+        case JS_TAG_OBJECT: {
+            auto value_ptr = reinterpret_cast<jlong>(JS_VALUE_GET_PTR(value));
+            jobject creatorObj = env->CallObjectMethod(thiz, creatorM);
+            if (JS_IsFunction(context, value)) {
+                auto obj_ptr = reinterpret_cast<jlong>(JS_VALUE_GET_PTR(this_obj));
+                result = env->CallObjectMethod(creatorObj, newFunctionM, thiz, value_ptr, obj_ptr);
+            } else if (JS_IsArray(context, value)) {
+                result = env->CallObjectMethod(creatorObj, newArrayM, thiz, value_ptr);
+            } else {
+                result = env->CallObjectMethod(creatorObj, newObjectM, thiz, value_ptr);
+            }
+            env->DeleteLocalRef(creatorObj);
+            break;
+        }
+
+        default:
+            result = nullptr;
+            break;
+    }
+
+    return result;
+}
+
 int main(){
     printf("Hello Quick JS");
     JSRuntime *pJsRuntime = jsNewRuntime(0,0);
